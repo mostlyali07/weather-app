9f1d6917cfec91b01ff43b72a4cde23b3621a9ec
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-09-17 02:10:40 +0500

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.getElementById("time").innerHTML += date;
 
 const referesh = () => {
     window.Geolocation.getCurrentPosition((position) => {
-        console.log(position.coords.latitude, position.coords.longitude)
+        console.log(position.coord.lat, position.coord.long);
     })
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=49da7703b2d50bab507896f3d5b56a7c`)
         .then((res) => res.json())
@@ -17,7 +17,7 @@ const referesh = () => {
 function getData() {
     let search = document.getElementById('citySearch').value.toLowerCase().trim()
         //Calling API
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=244006e372fb520e56e50b3032f7bcf1`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=49da7703b2d50bab507896f3d5b56a7c`)
         .then((res) => res.json())
         .then((res) => {
             showData(res)
