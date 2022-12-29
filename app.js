cb04aeec0a64d2722e47116794bedf031bb34550
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2022-12-29 22:09:42 +0500

    Update

--- app.js
+++ app.js
@@ -1,41 +1,37 @@
-// let date = new Date().toDateString();
-// document.getElementById("time").innerHTML += date;
-
-// const referesh = () => {
-//     window.Geolocation.getCurrentPosition((position) => {
-//         console.log(position.coord.lat, position.coord.long);
-//     })
-//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=49da7703b2d50bab507896f3d5b56a7c`)
-//         .then((res) => res.json())
-//         .then((res) => {
-//             showDate(res)
-//         }).catch((err) => {
-//             console.log(err)
-//         })
-// }
-
-// function getData() {
-//     let search = document.getElementById('citySearch').value.toLowerCase().trim()
-//         //Calling API
-//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=49da7703b2d50bab507896f3d5b56a7c`)
-//         .then((res) => res.json())
-//         .then((res) => {
-//             showData(res)
-//         }).catch((err) => {
-//             console.log(err)
-//         })
-// }
-
-
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'bcddf4ec12msh718a61341bd3869p139037jsn36f1f5fb4ab0',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "bcddf4ec12msh718a61341bd3869p139037jsn36f1f5fb4ab0",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
 };
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=karachi', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+const getWeather = (city) => {
+    cityName.innerHTML = city;
+  fetch(
+    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      cloud_pct.innerHTML = response.cloud_pct;
+      temp.innerHTML = response.temp;
+      feels_like.innerHTML = response.feels_like;
+      humidity.innerHTML = response.humidity;
+      min_temp.innerHTML = response.min_temp;
+      max_temp.innerHTML = response.max_temp;
+      wind_speed.innerHTML = response.wind_speed;
+      wind_degrees.innerHTML = response.wind_degrees;
+      sunrise.innerHTML = response.sunrise;
+      sunset.innerHTML = response.sunset;
+    })
+    .catch((err) => console.error(err));
+};
+
+submit.addEventListener("click", (e)=>{
+    e.preventDefault()
+    getWeather(city.value)
+})
+
+getWeather("Karachi")
